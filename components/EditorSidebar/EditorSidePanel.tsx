e4fb5d193d7210f59c2f3fbdc1c568e089241bc2
shin-hama <htysivista@gmail.com>
2022-12-22 11:22:09 +0900

    FEAT: Create side panel tabs

--- components/EditorSidebar/EditorSidePanel.tsx
+++ components/EditorSidebar/EditorSidePanel.tsx
@@ -1,14 +1,71 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
 import Stack from '@mui/material/Stack'
+import Icon from '@mui/material/Icon'
+
+type TabPanelProps = {
+  children?: React.ReactNode
+  index: number
+  value: number
+}
+
+const TabPanel: React.FC<TabPanelProps> = ({ children, value, index }) => {
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`side-panel-tabpanel-${index}`}
+      aria-labelledby={`side-panel-tab-${index}`}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  )
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `side-panel-tab-${index}`,
+    'aria-controls': `side-panel-tabpanel-${index}`,
+  }
+}
 
 const EditorSidePanel = () => {
+  const [value, setValue] = React.useState(0)
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue)
+  }
+
   return (
-    <Stack direction="row" height="100%" width="100%">
-      <Box bgcolor="black" color="white">
-        Images
+    <Stack direction="row" height="100%" width="100%" bgcolor="background.paper">
+      <Box color="white">
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          sx={{ borderRight: 1, borderColor: 'divider' }}
+        >
+          <Tab
+            icon={<Icon baseClassName="fas" className="fa-images" />}
+            label="Images"
+            {...a11yProps(0)}
+          />
+          <Tab icon={<Icon color="primary">add_circle</Icon>} label="Share" {...a11yProps(1)} />
+          <Tab icon={<Icon color="primary">add_circle</Icon>} label="Layout" {...a11yProps(1)} />
+        </Tabs>
       </Box>
-      <Box bgcolor="navy" sx={{ flexGrow: 1 }}></Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <TabPanel value={value} index={0}>
+          Images
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+      </Box>
     </Stack>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import type { AppProps } from 'next/app'
 import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
