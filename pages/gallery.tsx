3f1d8ba9cf8477614886574babfcf23743ce0ead
shin-hama <htysivista@gmail.com>
2022-12-30 17:28:39 +0900

    FEAT: Make thumbnails to be clickable

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -3,19 +3,33 @@ import Image from 'next/image'
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 
-const images = [1, 2, 3, 4, 5]
+const images = [1, 2, 3, 4, 5, 6]
 const Gallery = () => {
+  const [index, setIndex] = React.useState(0)
+
   return (
-    <Box width="100%" height="100%">
-      <Stack spacing={4} padding={2} alignItems="center">
-        <Box width="60%" bgcolor="black" sx={{ aspectRatio: '4/3' }}></Box>
-        <Stack spacing={2} direction="row">
-          {images.map((image) => (
-            <Box width={80} height={60} key={image} bgcolor="black" color="white">
-              {image}
-            </Box>
-          ))}
-        </Stack>
+    <Box width="100%" height="100vh" display="flex" alignItems="center" justifyContent="center">
+      <Stack spacing={4} padding={2} alignItems="start" width="100%">
+        <Box width="100%" bgcolor="black" color="white" sx={{ aspectRatio: '4/3' }}>
+          {images[index]}
+        </Box>
+
+        <Box display="flex" width="100%" overflow="auto">
+          <Stack spacing={2} direction="row">
+            {images.map((image, i) => (
+              <Box
+                width={100}
+                key={image}
+                bgcolor="black"
+                color="white"
+                onClick={() => setIndex(i)}
+                sx={{ aspectRatio: '4/3' }}
+              >
+                {image}
+              </Box>
+            ))}
+          </Stack>
+        </Box>
       </Stack>
     </Box>
   )
